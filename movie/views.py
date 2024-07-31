89a443a2b775afeddc0a8ed93d696d1fc7940915
DavidRamirez1106 <davidramirez.a4@gmail.com>
2024-07-31 12:10:05 -0500

    cambio de color

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'daramirez9'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to about Page</h1>')
+    return render(request, 'about.html')
