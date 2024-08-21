6cb9bc7a3eecb461898f97b68fb0dd331ee3c0ad
DavidRamirez1106 <davidramirez.a4@gmail.com>
2024-08-21 13:17:56 -0500

    peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=550)
-    image = models.ImageField(upload_to='movie/images/')
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=100)  # Título de la película
+    description = models.CharField(max_length=550)  # Descripción de la película
+    image = models.ImageField(upload_to='movie/images/')  # Imagen de la película
+    url = models.URLField(blank=True)  # URL opcional para más información
+    genre = models.CharField(blank=True, max_length=250)  # Género de la película
+    year = models.IntegerField(blank=True, null=True)  # Año de la película (opcional)
+
+    def __str__(self):
+        return self.title  # Retorna el título de la película como representación del objeto
 
